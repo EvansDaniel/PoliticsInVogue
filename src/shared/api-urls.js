cc0375d31fc68209fe7a652911df7c85e6b68c58
Daniel <evansdb0@sewanee.edu>
2017-12-30 20:39:39 -0600

    Updating api url

--- src/shared/api-urls.js
+++ src/shared/api-urls.js
@@ -1,5 +1,5 @@
 const API_BASE = '',
-    API_VERSION = '/v1';
+    API_VERSION = 'v1';
 
 let API_URLS_SHORTS = {
     article: 'article',
@@ -10,8 +10,10 @@ let API_URLS = {};
 
 for(let key in API_URLS_SHORTS) {
     if(API_URLS_SHORTS.hasOwnProperty(key)) {
-        API_URLS[key] = `${API_BASE}/${API_VERSION}/${API_URLS_SHORTS[key]}`
+        API_URLS[key] = `/${API_VERSION}/${API_URLS_SHORTS[key]}`
     }
 }
+
+console.log(API_URLS);
 
 module.exports = API_URLS;
